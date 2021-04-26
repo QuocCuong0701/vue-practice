4bb65b154ebc2208e1452af12c8fa5d59dea5d3c
QuocCuong <quoc.cuong.yb.dhtb@gmail.com>
2021-04-26 17:42:02 +0700

    up

--- learn-vuex/src/components/ProductList.vue
+++ learn-vuex/src/components/ProductList.vue
@@ -1,10 +1,7 @@
 <template>
     <div>
       <h1>Product List</h1>
-      <img
-        v-if="loading"
-        src="https://i.imgur.com/JfPpwOA.gif"
-      >
+      <img v-if="loading" src="https://i.imgur.com/JfPpwOA.gif" >
       <ul v-else>
         <li v-for="product in products">
           {{product.title}} - {{product.price | currency}} - {{product.inventory}}
@@ -45,7 +42,7 @@
     },
 
     created () {
-      this.loading = true
+      this.loading = true;
       this.fetchProducts()
         .then(() => this.loading = false)
     }
